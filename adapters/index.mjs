e3db709445f318e5b5a511b95a358a0e9335e68c
John Gully <john.gully@shopify.com>
2021-08-19 17:32:35 -0500

    Corrected error with adapter discovery

--- adapters/index.mjs
+++ adapters/index.mjs
@@ -1,8 +1,12 @@
+import path from "path";
 import importDirectory from "esm-import-directory";
 
 // Import all the modules in the csv and json paths.
 // This allows for automatic discovery of new adapters.
-export const jsonAdapters = await importDirectory("./adapters/json", { paths: true });
-// export const csvAdapters = await importDirectory("./adapters/csv", { paths: true }); 
+const dirname = path.dirname(import.meta.url.replace("file://", ""));
+const jsonAdapterPath = path.join(dirname, "json");
+export const jsonAdapters = await importDirectory(jsonAdapterPath, { paths: true });
+// const csvAdapterPath = path.join(dirname, "csv");
+// export const csvAdapters = await importDirectory(csvAdapterPath, { paths: true }); 
 
 export default jsonAdapters;
