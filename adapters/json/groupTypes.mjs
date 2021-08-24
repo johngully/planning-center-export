305f383404d3d8ea231bc6b00ca8d440de1967e3
John Gully <john.gully@shopify.com>
2021-08-24 16:31:29 -0500

    Whitespace cleanup

--- adapters/json/groupTypes.mjs
+++ adapters/json/groupTypes.mjs
@@ -3,4 +3,3 @@ import { createJsonAdapter } from "../planningCenterAdapterFactory.mjs";
 const url = "https://api.planningcenteronline.com/groups/v2/group_types";
 export const groupTypes = createJsonAdapter(url);
 export default groupTypes;
-
--- adapters/json/groups.mjs
+++ adapters/json/groups.mjs
@@ -3,4 +3,3 @@ import { createJsonAdapter } from "../planningCenterAdapterFactory.mjs";
 const url = "https://api.planningcenteronline.com/groups/v2/groups";
 export const groups = createJsonAdapter(url);
 export default groups;
-
